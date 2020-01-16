6c36ec73579b83a8db7545e6ed18abebac5444a5
Shivam <patelsd96@gmail.com>
2020-01-16 21:52:43 +0000

    added two conditionals to check for breed not found and status error codes

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,34 @@
 const request = require('request');
+//const fs = require('fs');
+
+
+const input = process.argv.slice(2);
+let URL = 'https://api.thecatapi.com/v1/breeds/search?q=';
+let searchTerm = input[0];
+
+//let filePath = input[1];
+
+// fetch the data from the url
+
+let URLFinal = URL + searchTerm;
+//console.log(URLFinal);
+request(URLFinal, (error, response, body) => {
+  const data = JSON.parse(body);
+  //console.log(JSON.parse(response.body).status)
+  if (data.length === 0) {
+    return console.log("cannot find the breed, please retry");
+  } else if (JSON.parse(response.body).status === 404) {
+    console.log("invalid url");
+  } else {
+    console.log(data[0].description);
+
+  }
+
+
+});
+
+
+
+
+
+// response.statusCode === 404
